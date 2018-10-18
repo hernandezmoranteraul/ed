79685b2aab3f76c7b8dd15e6d7073df0e1a8242e
hernandezmoranteraul <1dam.rhernandez@institutoserpis.org>
2018-10-18 12:16:17 +0200

    añadido JVector

---
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -0,0 +1,18 @@
+package org.institutoserpis.ed;
+
+public class Vector {
+	
+	public static int indexOf (int[] v , int x);{
+		return 
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] v= new int [] {16,12,15,14,17};
+		int x = 14;
+		int index = indexOf(v, 12);
+		System.out.println("index="+index);;
+		
+	}
+
+}
