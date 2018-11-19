6a2b433ace9aaf239b5facd16da64f0583b42b5f
hernandezmoranteraul <1dam.rhernandez@institutoserpis.org>
2018-11-19 09:45:07 +0100

    gitignore, CHolamundo m Csuma

---
+++ CHolamundo/CHolamundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CHolamundo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
---
+++ Csuma/Csuma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSuma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Primer número: ");
+            string stringPrimerNumero = Console.ReadLine();
+            //int primerNumero = int.Parse(stringPrimerNumero);
+            decimal primerNumero = decimal.Parse(stringPrimerNumero);
+            Console.Write("Segundo número: ");
+            string stringSegundoNumero = Console.ReadLine();
+            //int segundoNumero = int.Parse(stringSegundoNumero);
+            decimal segundoNumero = decimal.Parse(stringSegundoNumero);
+            //int suma = primerNumero + segundoNumero;
+            decimal suma = primerNumero + segundoNumero;
+            Console.Write(suma);
+
+        }
+    }
+}
