5363e122a720e82f96f43e5ce8b6dc4162b3a564
hernandezmoranteraul <1dam.rhernandez@institutoserpis.org>
2018-12-03 08:11:42 +0100

    Añadido Cvector propuesto BinarySearch

---
+++ CVector/CVector/NUnitTestClass.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void BinarySearch()
+        {
+            Assert.AreEqual(-2, Vector.BinarySearch(new int[] { 45 }, 33));
+        }
+    }
+}
---
+++ CVector/CVector/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CVector
+{
+    class Vector
+    {
+        public static void Main(string[] args)
+        {
+            int[] VectorArray = { 15, 26, 50, 65, 124, 357 };
+            Console.WriteLine(BinarySearch(VectorArray, 50));
+        }
+
+        public static int  BinarySearch (int[] v, int x) {
+            int count = v.Length;
+            if (count == 0)
+                return -1;
+            int left = 0;
+            int right = count - 1;
+            int middle = (left + right) / 2;
+            while (left < right && v[middle] != x)
+            {
+                if (v[middle] < x)
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+                middle = (left + right);
+            }
+
+            return 2;
+        }
+       
+    }
+}
