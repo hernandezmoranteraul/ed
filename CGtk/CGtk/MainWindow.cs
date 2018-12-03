a93300593b596fbf95f2fe30d5e8da92040aee40
hernandezmoranteraul <1dam.rhernandez@institutoserpis.org>
2018-12-03 09:53:59 +0100

    Añadido CGtk como introducción a Gtk con monodevelop

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButtonAceptarClicked(object sender, EventArgs e)
+    {
+  
+        string nombre = entryName.Text;
+        Console.WriteLine("Click en buttonAceptar con nombre=" + nombre);
+        labelSaludo.Text = "Hola " + nombre;
+    }
+}
